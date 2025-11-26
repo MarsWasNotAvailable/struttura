ac4f6bef2a2fa074afc26f8f83e03c815ca1364d
MarsWasNotAvailable <116720820+MarsWasNotAvailable@users.noreply.github.com>
2025-11-26 23:33:28 +0000

    new deploy YAML and some minor design code
    The application window is now set into a vertical resolution. Added a button click interaction. I'm not sure what changed or created the SLN file.

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
 
 namespace struttura;
 
@@ -8,4 +11,25 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine("Clicked the button");
+
+        if (sender is Button buttonCharacter)
+        {
+            DisplayInput.Text = buttonCharacter.Content?.ToString();
+            DisplayInput.FontStyle = Avalonia.Media.FontStyle.Normal;
+        }
+    }
+
+    protected override void OnResized(WindowResizedEventArgs e)
+    {
+        base.OnResized(e);
+
+        var b = Bounds;
+        if (b.Width > b.Height)
+            Width = b.Height / 1.6;
+    }
+
 }
